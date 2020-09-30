3593d4b7c13600199b3c2d1bc394935d51054fd6
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2020-09-30 16:41:23 +0300

    added mute operator

--- src/compile.test.ts
+++ src/compile.test.ts
@@ -1,49 +1,87 @@
 import { marbles } from 'rxjs-marbles/jest';
-import { many, query } from './compile';
+import { many, mute, query } from './compile';
 
 describe('Compilation', () => {
 
-    test('A', marbles(m => {
-        const A = m.hot('^-1-2-3-|')
-        const result  = query(A);
-        const expected = '^-(1|)'
+    describe('ABC', () => {
+        test('A', marbles(m => {
+            const A = m.hot('^-1-2-3-|')
+            const result = query(A);
+            const expectd = '^-(1|)'
 
-        m.expect(result).toBeObservable(expected);
-    }))
+            m.expect(result).toBeObservable(expectd);
+        }))
 
-    test('AB', marbles(m => {
-        const A = m.hot('^-1-----|')
-        const B = m.hot('^---2---|')
-        const expected ='^-1-(2|)'
-        const result = query(A, B);
-        m.expect(result).toBeObservable(expected);
-    }));
+        test('AB', marbles(m => {
+            const A = m.hot('^-1-----|')
+            const B = m.hot('^---2---|')
+            const expectd = '^-1-(2|)'
+            const result = query(A, B);
+            m.expect(result).toBeObservable(expectd);
+        }));
+
+        test('ABC', marbles(m => {
+            const A = m.hot('^----0----------|')
+            const B = m.hot('^-1-2-3-4-5-6-7-|')
+            const C = m.hot('^------------0--|')
+            const expectd = '^----03------(0|)'
+            const result = query(A, B, C);
+            m.expect(result).toBeObservable(expectd);
+        }));
+    })
 
     describe('AB*C', () => {
         test('A*', marbles(m => {
             const A = m.hot('^-1-2-3-|')
-            const expected ='^-1-2-3-|'
+            const expectd = '^-1-2-3-|'
             const result = query(many(A));
-            m.expect(result).toBeObservable(expected);
-		}));
+            m.expect(result).toBeObservable(expectd);
+        }));
 
         test('A*B', marbles(m => {
             const A = m.hot('^-1-2-3-|')
             const B = m.hot('^----0--|')
-            const expected ='^-1-2(0|)'
-			const result = query(many(A), B);
-            m.expect(result).toBeObservable(expected);
-		}));
+            const expectd = '^-1-2(0|)'
+            const result = query(many(A), B);
+            m.expect(result).toBeObservable(expectd);
+        }));
 
         test('AB*C', marbles(m => {
-			const A = m.hot('^----0----------|')
-			const B = m.hot('^-1-2-3-4-5-6-7-|')
-			const C = m.hot('^------------0--|')
-			const expected= '^----03-4-5-6(0|)'
-			const result = query(A, many(B), C);
-            m.expect(result).toBeObservable(expected);
+            const A = m.hot('^----0----------|')
+            const B = m.hot('^-1-2-3-4-5-6-7-|')
+            const C = m.hot('^------------0--|')
+            const expectd = '^----03-4-5-6(0|)'
+            const result = query(A, many(B), C);
+            m.expect(result).toBeObservable(expectd);
         }));
     });
 
+    describe('_AB*_C', () => {
+        test('_A', marbles(m => {
+            const A = m.hot('^-1-2-3-|')
+            const expectd = '^-|'
+            const result = query(mute(A));
+
+            m.expect(result).toBeObservable(expectd);
+        }));
+
+        test('_AB', marbles(m => {
+            const A = m.hot('^-1-----|')
+            const B = m.hot('^---2---|')
+            const expectd = '^---(2|)'
+            const result = query(mute(A), B);
+
+            m.expect(result).toBeObservable(expectd);
+        }));
+
+        test('_AB*_C', marbles(m => {
+            const A = m.hot('^----0----------|')
+            const B = m.hot('^-1-2-3-4-5-6-7-|')
+            const C = m.hot('^------------0--|')
+            const expectd = '^-----3-4-5-6|'
+            const result = query(mute(A), many(B), mute(C));
+            m.expect(result).toBeObservable(expectd);
+        }));
+    });
 })
 
--- src/compile.ts
+++ src/compile.ts
@@ -11,78 +11,128 @@ function _collectDeps(nodes: any[]) {
     return deps;
 }
 
-function compileNode(node) {
+type InputValue<T> = Observable<T> | INode<T>;
+
+enum NodeType {
+    one = 'one',
+    many = 'many',
+    group = 'group',
+    mute = 'mute',
+}
+
+interface INode<A = any> {
+    type: NodeType;
+    deps: Set<Observable<A>>;
+    output: Subject<A>; // TODO: make it Observable<A>
+    handleEmission(o: Observable<A>, value: A): void;
+    matchesEmission(o: Observable<A>): boolean;
+}
+
+function compileNode<T>(node: InputValue<T>): INode {
     if (isObservable(node)) {
-        return {
-            type: 'STREAM',
-            deps: new Set([node]),
-            matchesEmission: s => s === node
-        }
+        return one(node);
     }
 
     return node;
 }
 
-export function many(..._nodes) {
+export function one<T>(o: Observable<T>): INode<T> {
+    const output = new Subject<T>();
+    const deps = new Set<Observable<T>>();
+    deps.add(o);
+
+    return {
+        type: NodeType.one,
+        deps,
+        output,
+        handleEmission: (_, v) => {
+            output.next(v);
+        },
+        matchesEmission: s => s === o
+    }
+}
+
+export function many<T>(..._nodes: InputValue<T>[]): INode<T> {
     const node = compileGroup(_nodes);
 
     return {
-        type: 'REPEAT',
+        type: NodeType.many,
         deps: node.deps,
+        output: node.output,
+        handleEmission: node.handleEmission,
         matchesEmission: node.matchesEmission
     }
 }
 
-function compileGroup(_nodes: any[]) {
-    const output = new Subject();
-    let currIndex = 0;
+export function mute<T>(..._nodes: InputValue<T>[]): INode<T> {
+    const group = compileGroup(_nodes);
+
+    return {
+        type: NodeType.mute,
+        deps: group.deps,
+        output: new Subject<T>(),
+        handleEmission: () => void 0, // do nothing
+        matchesEmission: group.matchesEmission
+    }
+}
+
+function compileGroup<T>(_nodes: InputValue<T>[]): INode<T> {
+    const output = new Subject<T>();
     const nodes = _nodes.map(compileNode);
     const deps = _collectDeps(nodes);
 
-    function handleEmission(key, value) {
-        const currNode = nodes[currIndex];
-        let completed = false;
+    // iteration state
+    let currIndex = 0;
 
-        if (currNode.type == 'STREAM') {
-            if (currNode.matchesEmission(key)) {
-                output.next(value);
-                currIndex += 1;
-            }
-        }
+    function handleEmission(o: Observable<T>, value) {
+        const node = nodes[currIndex];
 
-        if (currNode.type == 'REPEAT') {
+        // checking for greedy MANY operator
+        if (node.type == NodeType.many) {
             const nextNode = nodes[currIndex + 1];
-            if (nextNode && nextNode.matchesEmission(key)) {
+            if (nextNode && nextNode.matchesEmission(o)) {
                 currIndex++;
-                return handleEmission(key, value);
-            } else if (currNode.matchesEmission(key)) {
+                return handleEmission(o, value);
+            } else if (node.matchesEmission(o)) {
                 output.next(value);
+            }
+        } else {
+            if (node.matchesEmission(o)) {
+                const sub = node.output.subscribe(v => {
+                    output.next(v);
+                });
+                node.handleEmission(o, value);
+                sub.unsubscribe();
+                currIndex += 1;
             }
         }
 
         // check if capturing group should be completed
         if (currIndex > nodes.length - 1) {
             output.complete();
-            completed = true;
         }
-
-        return { completed };
     }
 
-    function matchesEmission() {
-        return true;
+    function matchesEmission(o) {
+        // Emtpy group matches any emission
+        if (nodes.length == 0) {
+            return true;
+        }
+
+        // check if first value matches
+        return nodes[0].matchesEmission(o);
     }
 
     return {
-        type: 'GROUP',
-        matchesEmission,
+        type: NodeType.group,
         deps,
         output,
+        matchesEmission,
         handleEmission
     }
 }
 
-function query(...values: any[]) {
+function query<T>(...values: InputValue<T>[]) {
     const rootGroup = compileGroup(values);
 
     const entries = Array.from(rootGroup.deps.values(), stream => {
